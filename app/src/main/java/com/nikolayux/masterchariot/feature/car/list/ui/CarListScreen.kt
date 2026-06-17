2f188b49e31f76c5d7564fed08711af90f486e87
Nikolay-ux <nikolael.pochta@gmail.com>
2026-06-17 17:30:44 +0700

    Optimize import

--- app/src/main/java/com/nikolayux/masterchariot/feature/car/list/ui/CarListScreen.kt
+++ app/src/main/java/com/nikolayux/masterchariot/feature/car/list/ui/CarListScreen.kt
@@ -33,7 +33,6 @@ import androidx.compose.material.icons.filled.LightMode
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.NotificationsOff
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
