107225db3aeb8bbd5f783536b203e0eeab583927
Nikolay-ux <nikolael.pochta@gmail.com>
2026-06-17 14:45:56 +0700

    Edit default theme colors

--- app/src/main/java/com/nikolayux/masterchariot/ui/theme/Color.kt
+++ app/src/main/java/com/nikolayux/masterchariot/ui/theme/Color.kt
@@ -2,15 +2,6 @@ package com.nikolayux.masterchariot.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-
 //Dark theme colors
 val Grey1 = Color(0xFF1B1B1B)
 
@@ -29,6 +20,8 @@ val Blue1 = Color(0xFF0084D1)
 val Blue2 = Color(0xFF398EBF)
 
 val Blue3 = Color(0xFF4CBDFF)
+
+val Blue5 = Color(0xFF325367)
 
 val White = Color(0xFFFFFFFF)
 
